22edfdf1fb761502d9e72bebddfc8149732e8e13
axoletl <38306961+axoletl@users.noreply.github.com>
2022-07-23 14:42:06 +1000

    style

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <template>
   <header>
-    <h1>Iris Three + Vue Starter</h1>
-    <br />
-    <h2>(+Webpack and Sass)</h2>
+    <div class="b-container">
+      <h1>Iris Three + Vue Starter</h1>
+      <br />
+      <h2>(+Webpack and Sass)</h2>
+    </div>
   </header>
   <main>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum
-    </p>
-    <MainCanvas />
-    <div style="height: 200vh; border: 1px solid black; margin: 1rem; padding: 1rem;">
+	<MainCanvas />
+    <div class="b-container">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+      </p>
+    </div>
+    <div
+      class="b-container"
+      style="
+        height: 200vh;
+        border: 1px solid black;
+        margin: 1rem auto;
+        padding: 1rem;
+      "
+    >
       <h3>Scroll test div</h3>
     </div>
   </main>
@@ -25,18 +37,23 @@
 <script>
 import MainCanvas from './components/MainCanvas.vue'
 export default {
-	name: 'App',
-	components: {
-		MainCanvas,
-	},
-	methods: {
+  name: 'App',
+  components: {
+    MainCanvas,
+  },
+  methods: {
 
-	}
+  }
 }
 </script>
 
 <style lang="scss">
-@use "./styles/vars.scss";
-@use "./styles/reset.scss";
-@use "./styles/typography.scss";
+@import "./styles/vars.scss";
+@import "./styles/reset.scss";
+@import "./styles/typography.scss";
+
+.b-container {
+  max-width: $b-lg;
+  margin: 0 auto;
+}
 </style>
